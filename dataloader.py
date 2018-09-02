16d1f8af48d5b4d8ce5f4876ef91872d7227f42d
MrLiglog <tsmn82@gmail.com>
2018-09-02 16:48:30 +0800

    added sample data; updated filereader

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,5 @@
+#File to handles the importing of data
+
 import pandas as pd
 
 class dataLoader:
@@ -5,7 +7,11 @@ class dataLoader:
     #function to load data from an excel/csv
     #path is a string filepath
     def loadData(path):
-        file = pd.read_excel(path)
+        try:
+            file = pd.read_excel(path)
+        except:
+            print("File Read Error - Check Path")
+            exit(1)
 
 dl = dataLoader()
 dl.loadData("sample.xlsx")
