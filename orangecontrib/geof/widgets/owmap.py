d6e9fb7828d3c5afe93512422b43a86c228b802d
jonloh714 <42117144+jonloh714@users.noreply.github.com>
2018-10-16 16:53:47 +0800

    Prepare for building
    Added copyright notices and license headers to owmap to comply with GPLv3.
    
    Added setup file to build distribution files
    
    Removed wxFormBuilder stuff
    
    Moved docs from original geo over
    Updated gitignore

--- orangecontrib/geof/widgets/owmap.py
+++ orangecontrib/geof/widgets/owmap.py
@@ -1,3 +1,22 @@
+Copyright 2018 CITS3200 2018 Group C, University of Western Australia
+Copyright (c) 2016 Bioinformatics Laboratory, University of Ljubljana,
+  Faculty of Computer and Information Science
+
+ This file is part of Geo.
+
+    Geo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Geo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Geo.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from itertools import chain, repeat
 from collections import OrderedDict
